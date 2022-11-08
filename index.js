9865c52eb3d84bffe7129ba54b0e53997cc04d38
Rakib Ahmed <sujonahmed45a4@gmail.com>
2022-11-08 18:45:21 +0600

    services post api method completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -30,16 +30,51 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
+
+app.post('/services', async (req, res) => {
+    try {
+        const service = req.body;
+        const result = await Services.insertOne(service);
+
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: 'Successfully insert the service',
+                data: result
+            })
+        } else {
+            res.send({
+                success: false,
+                error: 'Could not insert the service'
+            })
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.secure(error.message)
+    }
+})
+
+
 app.get('/services', async (req, res) => {
     try {
-        const query = {};
-        const cursor = Services.find(query);
-        const services = await cursor.toArray();
-        res.send({
-            success: true,
-            message: 'Successfully got the services',
-            data: services
-        })
+        const limit = parseInt(req.query.limit);
+
+        if (limit) {
+            const services = await Services.find({}).limit(limit).toArray();
+            res.send({
+                success: true,
+                message: 'Successfully got the services',
+                data: services
+            });
+        } else {
+            const services = await Services.find({}).toArray();
+            res.send({
+                success: true,
+                message: 'Successfully got the services',
+                data: services
+            });
+        }
+
     } catch (error) {
         console.log(error.message);
         res.send({
@@ -49,8 +84,23 @@ app.get('/services', async (req, res) => {
     }
 });
 
-app.get('/services/:id', (req, res) => {
-    const id = req.params.id;
+app.get('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await Services.findOne(query);
+        res.send({
+            success: true,
+            message: 'Successfully got the service',
+            data: service
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
 })
 
 app.listen(port, () => { console.log(`server is running on port ${port}`) })
